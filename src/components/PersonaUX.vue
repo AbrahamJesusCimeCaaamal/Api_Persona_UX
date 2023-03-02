e8f7b058089133255840f5d27759317d48762a20
unknown <abraham.19070019@itsmotul.edu.mx>
2023-03-02 08:24:16 -0600

    Codigo inicial con talwind

---
+++ src/components/PersonaUX.vue
@@ -0,0 +1,43 @@
+<script>
+import axios from 'axios'
+
+
+export default {
+
+  methods: {
+ RegistroUX(){
+  axios
+.post("/api/guardarPersonasUxd.php", {
+nombre: this.nombre,
+objetivos:this.objetivos,
+frustraciones: this.frustraciones,
+motivaciones: this.motivaciones,
+marcas: this.marcas,
+//completar las variables, estas deben llamarse como las que se recibirán en el backend sin el símbolo del dolar $
+})
+.then((response) => {
+console.log(response.status)
+});
+  
+
+ }
+   
+  },
+
+  }
+
+  
+
+
+
+</script>
+
+<template>
+
+
+
+
+
+
+ 
+</template>
