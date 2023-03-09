71cabf9f4958b902bc02d7d91c6d2a87e0da9811
unknown <abraham.19070019@itsmotul.edu.mx>
2023-03-08 23:08:25 -0600

    ventana de inicio

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -3,11 +3,23 @@
 <template><br>
   
   <div class=" bg-lime-100 font-sans  ">
+ <h1 class="text-4xl text-center font-bold px-2 py-4 "  > Personas en UX. Conoce y empatiza con los usuarios </h1><br>  
+ <p class="text-xl px-4 py-2"> La técnica de construcción de personas en UX nos ayuda a definir los perfiles de usuario cuando se trata de
+   conocer rasgos característicos del público objetivo de un sitio web o aplicación.</p><br>
 
+ <p class="text-xl px-4 py-2">
+  Conocer, comprender, entender y empatizar con los usuarios de un sistema es 
+  clave para que tenga éxito. La técnica de construcción de personas es una de 
+  las más utilizadas en el ámbito HCI y UCD (Diseño Centrado en el Usuario).
+ </p> <br>
+
+ <p class="text-xl px-4 py-2">Ayuda a que el diseñador UX del producto digital entienda
+ para quien está diseñado el producto, en la toma de decisiones 
+ y a no perder el foco en los objetivos del proyecto y las necesidades de los usuarios.</p> <br>
 
   <div class="grid grid-cols-2 ">
   <div class=" w-full p-2 text-center bg-blue-600 lg:px-48 lg:py-40 md:px-14 md:py-80  ">
-    
+    <img class="scale-150 py-16 " src="../components/icons/principal.png" width="500" height="500" >
 
   </div>
 
@@ -15,7 +27,19 @@
 
   <div class=" font-semibold w-full p-2 text-center bg-blue-300 sm:px-2 sm:py-36 md:px-24 md:py-36 lg:px-24 lg:py-14">
     
-  
+  <h1 class="text-4xl font-bold text-black "> Persona UX</h1><br>
+ <h1 class="text-2xl">¿Qué es Persona?</h1><br>
+  <p class="text-xl text-justify "> 
+    
+Persona se refiere a uno o unos personajes ficticios creados con 
+motivo de representar a los usuarios posibles de un producto a través 
+de una buena cantidad de investigaciones cualitativas y cuantitativas.
+ Persona nos contesta la pregunta “¿Para quién diseñamos?” Es una herramienta 
+ poderosa basada en conclusiones de investigaciones en ayudar la creación de funciones
+  de producto optimizando la investigación de UX. No sólo representa a algún usuario 
+  concreto sino al conjunto de ellos, se puede entender como el personaje típico de la 
+  conducta, la actitud, las habilidades y los contextos de todos los usuarios potenciales.
+  </p>
 
   </div>
 
